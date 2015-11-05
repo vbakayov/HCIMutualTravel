17d9660dd678d462e44eb7995ef465753470331d
vbakayov <viktorbakayov@gmail.com>
2015-11-05 22:05:24 +0000

    Material design tabs implementation
    Unwanted Behavior : luggage tab due to its size splits on two lines

---
+++ app/src/main/java/samples/exoguru/materialtabss/Tab1.java
@@ -0,0 +1,20 @@
+package samples.exoguru.materialtabss;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Viktor on 11/5/2015.
+ */
+public class Tab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_1,container,false);
+        return v;
+    }
+}
---
+++ app/src/main/java/samples/exoguru/materialtabss/ViewPagerAdapter.java
@@ -0,0 +1,55 @@
+package samples.exoguru.materialtabss;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Viktor on 11/5/2015.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+
+    // Build a Constructor and assign the passed Values to appropriate values in the class
+    public ViewPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
+        super(fm);
+
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+
+    }
+
+    //This method return the fragment for the every position in the View Pager
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: Tab1 tab1 = new Tab1();
+                    return tab1;
+            case 1:  Tab2 tab2 = new Tab2();
+                    return tab2;
+            case 2:  Tab3 tab3 = new Tab3();
+                    return tab3;
+            case 3:  Tab4 tab4 = new Tab4();
+                    return tab4;
+        }
+        return null;
+    }
+
+    // This method return the titles for the Tabs in the Tab Strip
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    // This method return the Number of tabs for the tabs Strip
+
+    @Override
+    public int getCount() {
+        return NumbOfTabs;
+    }
+}
